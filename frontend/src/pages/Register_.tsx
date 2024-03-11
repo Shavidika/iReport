565a3c7ed63c1d6646ba9be695a6c9b59fe44167
Themal Lakruwan <themaldesilva2001@gmail.com>
2024-03-11 11:37:10 +0530

    register page

--- frontend/src/pages/Register_.tsx
+++ frontend/src/pages/Register_.tsx
@@ -4,7 +4,7 @@ import Map from "../Assets/World_dot_map.png";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../hooks/redux-hooks";
-import { login } from "../slices/authSlice";
+import { register } from "../slices/authSlice";
 import { Console } from "console";
 
 import {
@@ -12,17 +12,23 @@ import {
     NotificationType,
   } from "../slices/notificationSlice";
 
-const LoginPage: React.FC = () => {
+const SignupPage: React.FC = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
+  const [passwordConf, setPasswordConf] = useState("");
+  const [passwordConfError, setPasswordConfError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const roles = ["user"];
+  const userImage = "https://i.ibb.co/yWsXMvm/User-Image.png";
 
   const handleLogin = async (event: React.FormEvent) => {
     setEmailError("");
+    setPasswordConf("");
     setPasswordError("");
 
     // Check if the user has entered both fields correctly
@@ -46,19 +52,27 @@ const LoginPage: React.FC = () => {
       return;
     }
 
-    if (email && password) {
+    if (password !== passwordConf) {
+      setPasswordConfError("Passwords do not match");
+      return;
+    }
+
+    if (name && email && password) {
       try {
         await dispatch(
-          login({
+          register({
+            name,
             email,
             password,
+            roles,
+            userImage,
           })
         ).unwrap();
-        navigate("/profile");
+        navigate("/login");
       } catch (e) {
         setPasswordError("Invalid email or password");
         console.error(e);
-        navigate("/login");
+        navigate("/register");
       }
     } else {
       console.log("there is an Error");
@@ -81,9 +95,27 @@ const LoginPage: React.FC = () => {
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Sign in to your account
+              create your account
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  htmlFor="text"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Your name
+                </label>
+                <input
+                  type="text"
+                  name="text"
+                  id="text"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="Enter your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -124,6 +156,27 @@ const LoginPage: React.FC = () => {
                 />
                 <label className="text-red-600 text-xs">{passwordError}</label>
               </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required
+                  value={passwordConf}
+                  onChange={(e) => {
+                    setPasswordConf(e.target.value);
+                  }}
+                />
+                <label className="text-red-600 text-xs">{passwordConfError}</label>
+              </div>
               <div className="flex items-center justify-between">
                 <div className="flex items-start">
                   <div className="flex items-center h-5">
@@ -145,7 +198,7 @@ const LoginPage: React.FC = () => {
                   href="#"
                   className="text-sm font-medium text-primary-600 hover:underline"
                 >
-                  Forgot password?
+                 
                 </a>
               </div>
               <button
@@ -153,15 +206,15 @@ const LoginPage: React.FC = () => {
                 className="w-full text-black bg-primary-600 bg-slate-100 hover:bg-slate-300 focus:ring-4 focus:outline-1 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 onClick={handleLogin}
               >
-                Log In
+                Sign Up
               </button>
               <p className="text-sm font-light text-gray-500">
-                Don’t have an account yet?{" "}
+                Already have an account?{" "}
                 <Link
                   to="/register"
                   className="font-medium text-primary-600 hover:underline"
                 >
-                  Sign up
+                  Log in
                 </Link>
               </p>
             </form>
@@ -172,4 +225,4 @@ const LoginPage: React.FC = () => {
   );
 };
 
-export default LoginPage;
+export default SignupPage;
